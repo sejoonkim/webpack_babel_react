bbff346f02a0782de5571b49c2a85a06fbdf20c9
sejoonkim <sejoondev@gmail.com>
2020-07-07 22:39:29 +0900

    4. 리액트 컴포넌트 개발

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
             loader: "css-loader",
             options: {
               modules: true,
-              camelCase: true,
               sourceMap: true,
             },
           },
